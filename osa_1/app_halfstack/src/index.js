6b2ed4adba066004a1616353a18cd220ae9c08a4
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-01-26 13:40:59 +0200

    1.1 kaunisteltu

--- osa_1/app_halfstack/src/index.js
+++ osa_1/app_halfstack/src/index.js
@@ -1,6 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Otsikko = (props) => {
+  return (
+    <h1>{props.kurssi}</h1>
+  )
+}
+
+const Sisalto = (props) => {
+  return (
+      <text>{props.sisalto}</text>
+  )
+}
+
+const Yhteensa = (props) => {
+  return (
+      <text>{props.tehtavia}</text>
+  )
+}
+
 const App = () => {
   const kurssi = 'Half Stack -sovelluskehitys'
   const osa1 = 'Reactin perusteet'
@@ -12,11 +30,11 @@ const App = () => {
 
   return (
     <div>
-      <h1>{kurssi}</h1>
-      <p>{osa1} {tehtavia1}</p>
-      <p>{osa2} {tehtavia2}</p>
-      <p>{osa3} {tehtavia3}</p>
-      <p>yhteensä {tehtavia1 + tehtavia2 + tehtavia3} tehtävää</p> 
+      <Otsikko kurssi = {kurssi}/>
+      <p><Sisalto sisalto = {osa1}/> <Yhteensa tehtavia = {tehtavia1}/></p>
+      <p><Sisalto sisalto = {osa2}/> <Yhteensa tehtavia = {tehtavia2}/></p>
+      <p><Sisalto sisalto = {osa3}/> <Yhteensa tehtavia = {tehtavia3}/></p>
+      <p>yhteensä <Yhteensa tehtavia = {tehtavia1 + tehtavia2 + tehtavia3}/>tehtävää</p> 
     </div>
   )
 }
