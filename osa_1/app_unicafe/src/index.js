eee92616ce5eeae1a3712f2fbaa8a8b1bebbbf60
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-01-26 15:35:59 +0200

    1.6 html structure

--- osa_1/app_unicafe/src/index.js
+++ osa_1/app_unicafe/src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+const App = (props) => {
+
+  return (
+    <div>
+      <h1>Anna palautetta</h1>
+      <button value="hyva">hyva</button> 
+      <button value="neutraali">neutraali</button> 
+      <button value="huono">huono</button> 
+      <h1>Statistiikka</h1>
+      <table>
+        <tbody>
+          <tr>
+            <td>hyvä</td>
+            <td>6</td>
+          </tr>
+          <tr>
+            <td>neutraali</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>huono</td>
+            <td>2</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
