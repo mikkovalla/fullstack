4b0b21d76088e7d091462acf60c79c434d2d164f
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-01-28 14:14:47 +0200

    1.6 done

--- osa_1/app_unicafe/src/index.js
+++ osa_1/app_unicafe/src/index.js
@@ -2,34 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = (props) => {
+class App extends React.Component {
+  constructor () {
+    super ()
+    this.state = {
+      hyva: 0,
+      huono: 0,
+      neutraali: 0
+    }
+  }
 
-  return (
+  render(){
+    return (
     <div>
       <h1>Anna palautetta</h1>
-      <button value="hyva">hyva</button> 
-      <button value="neutraali">neutraali</button> 
-      <button value="huono">huono</button> 
+      <button onClick={() => this.setState({hyva: this.state.hyva + 1})}>hyva</button> 
+      <button onClick={() => this.setState({huono: this.state.huono + 1})}>neutraali</button> 
+      <button onClick={() => this.setState({neutraali: this.state.neutraali + 1})}>huono</button> 
       <h1>Statistiikka</h1>
       <table>
         <tbody>
           <tr>
             <td>hyvä</td>
-            <td>6</td>
+            <td>{this.state.hyva}</td>
           </tr>
           <tr>
             <td>neutraali</td>
-            <td>3</td>
+            <td>{this.state.huono}</td>
           </tr>
           <tr>
             <td>huono</td>
-            <td>2</td>
+            <td>{this.state.neutraali}</td>
           </tr>
         </tbody>
       </table>
     </div>
-
   )
+  }
 }
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
