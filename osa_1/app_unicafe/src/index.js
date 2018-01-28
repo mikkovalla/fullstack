5b61c485219159a0f3be7191a1b176a157003330
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-01-28 15:14:58 +0200

    1.7 done (ei NaN chekkiä...)

--- osa_1/app_unicafe/src/index.js
+++ osa_1/app_unicafe/src/index.js
@@ -8,17 +8,22 @@ class App extends React.Component {
     this.state = {
       hyva: 0,
       huono: 0,
-      neutraali: 0
-    }
+      neutraali: 0,
+      keskiarvo: 0,
+      positiivisia: 0,
+      statPos: 0,
+      statKes: 0,
+      statAll: 0
+    } 
   }
 
-  render(){
+  render (){
     return (
     <div>
       <h1>Anna palautetta</h1>
-      <button onClick={() => this.setState({hyva: this.state.hyva + 1})}>hyva</button> 
-      <button onClick={() => this.setState({huono: this.state.huono + 1})}>neutraali</button> 
-      <button onClick={() => this.setState({neutraali: this.state.neutraali + 1})}>huono</button> 
+      <button onClick={() => this.setState({hyva: this.state.hyva + 1, statPos: this.state.statPos + 1, statAll: this.state.statAll + 1})}>hyva</button> 
+      <button onClick={() => this.setState({huono: this.state.huono + 1, statPos: this.state.statPos - 1, statAll: this.state.statAll + 1})}>neutraali</button> 
+      <button onClick={() => this.setState({neutraali: this.state.neutraali + 1, statAll: this.state.statAll + 1})}>huono</button> 
       <h1>Statistiikka</h1>
       <table>
         <tbody>
@@ -33,6 +38,14 @@ class App extends React.Component {
           <tr>
             <td>huono</td>
             <td>{this.state.neutraali}</td>
+          </tr>
+          <tr>
+            <td>keskiarvo</td>
+            <td>{Math.round((this.state.statPos / this.state.statAll) * 10) / 10}</td>
+          </tr>
+          <tr>
+            <td>positiivisia</td>
+            <td>{Math.round(((this.state.statPos / this.state.statAll) * 100) * 10) / 10} %</td>
           </tr>
         </tbody>
       </table>
