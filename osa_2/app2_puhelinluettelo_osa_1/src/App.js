edee99e0b7a5b1c1931d61c7e03484ecb4a998df
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-02 23:38:19 +0200

    2.6 done

--- osa_2/app2_puhelinluettelo_osa_1/src/App.js
+++ osa_2/app2_puhelinluettelo_osa_1/src/App.js
@@ -1,21 +1,73 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+//import Numerot from './components/Numerot'
 
-class App extends Component {
-  render() {
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      persons: [
+        {name: 'Arto Hellas'}
+      ],
+      newName: ''
+    }
+  }
+
+  kaikkiNimet (persons) {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        this.state.persons.map(person =>
+        <p key = {person.name}> {person.name}</p>
+        )
+    )
+  }
+
+  lisaaNimi = (event) => {
+    event.preventDefault()
+    const uusiPersoona = {
+      name: this.state.newName,
+      id: Math.random()
+    }
+    console.log('uusin lisäys', uusiPersoona)
+    const persons = this.state.persons.concat(uusiPersoona)
+    console.log('persoonat', persons)
+
+    this.setState ({
+      persons,
+      newName: ''
+    })
+  }
+
+  handleNameAdd = (event) => {
+    console.log('event', event.target.value)
+    this.setState ({ 
+      newName: event.target.value
+    })
+  }
+
+  render () {
+    return (
+      <div>
+
+        <h2>Puhelinluettelo</h2>
+        
+        <form onSubmit={this.lisaaNimi}>
+          
+          <div>
+            nimi: <input value={this.state.newName} onChange={this.handleNameAdd}/>
+          </div>
+          
+          <div>
+            <button type="submit">Lisää</button>
+          
+          </div>
+        </form>
+
+        <h2>Numerot</h2>
+        <div>
+         {this.kaikkiNimet()}
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ osa_2/app2_puhelinluettelo_osa_1/src/components/Numerot.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Numerot = ({ numerot }) => {
+  return (
+    <li>numerot.name</li>
+  )
+}
+
+export default Numerot
