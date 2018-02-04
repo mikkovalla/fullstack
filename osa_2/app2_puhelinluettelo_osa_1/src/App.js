8da1c15239792200975311cf538ea04996b84df7
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-04 14:59:27 +0200

    2.11 fetch data done

--- osa_2/app2_puhelinluettelo_osa_1/src/App.js
+++ osa_2/app2_puhelinluettelo_osa_1/src/App.js
@@ -1,20 +1,29 @@
 import React from 'react'
 import FilleroidutNimet from './components/FilleroidutNimet'
+import axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      persons: [
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Martti Tienari', number: '040-123456' },
-        { name: 'Arto Järvinen', number: '040-123456' },
-        { name: 'Lea Kutvonen', number: '040-123456' }
-      ],
+      persons: [],
       newName: '',
       newNumb: '',
       filter: ''
     }
+  }
+
+  componentDidMount () {
+    console.log('did mount')
+    axios
+    .get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('vastaus', response)
+      this.setState({
+        persons: response.data
+      })
+    })
+    
   }
 
   lisaaHenkilo = (event) => {
