750d5e1ba01937ceead492ecdb2de0eba7969759
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-02-04 16:30:57 +0200

    2.12 done

---
+++ osa_2/app3_countries/src/App.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import './App.css'
+import axios from 'axios'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      countries: [],
+      filter: ''
+    }
+  }
+
+  componentDidMount () {
+    console.log('did mount')
+    axios
+    .get('https://restcountries.eu/rest/v2/all?fields=name;flag;capital;population')
+    .then(response => {
+      console.log('response', response.data)
+      this.setState ({
+        countries: response.data
+      })
+    })
+  }
+
+  handleInput = (event) => {
+    console.log('input', event.target.value)
+    this.setState({
+      filter: event.target.value
+    })
+  }
+
+  handleFiltering (countries) {
+
+    const filterCountries = this.state.countries.filter(maa => maa.name.substring(0, this.state.filter.length).toUpperCase() === this.state.filter.substring(0, this.state.filter.length).toUpperCase())
+    console.log('maita', filterCountries)
+    console.log('pituus', filterCountries.length)
+    
+    if(filterCountries.length < 10) {
+      if(filterCountries.length === 1) {
+        return (
+          filterCountries.map(maa =>
+          <div key = {maa.name}>
+          <h5>{maa.name}</h5>
+            <div>
+              <p>capital: {maa.capital}</p>
+              <br/>
+              <p>population: {maa.population}</p>
+              <br/>
+              <img src={maa.flag} alt="maan lippu"/>
+            </div>
+          </div>
+          )
+        )
+      }
+      return (
+        filterCountries.map(maa => 
+        <p key= {maa.name}>{maa.name}</p>
+        )
+      )        
+    } else {
+      return (
+        <p>Too many matches, please add another letter</p>
+      )
+    }      
+  }
+
+  render () {
+    console.log('maita', this.state.countries)
+    return (
+      <div>
+        <div>
+          Find countries: <input value={this.state.filter} onChange={this.handleInput} />
+        </div>
+        <h4>Countries</h4>
+        <div>
+          {this.handleFiltering()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default App;
