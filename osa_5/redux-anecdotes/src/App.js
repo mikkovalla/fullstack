4bdb35a7ccd336187c1706d33812c482f92709dc
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-03-04 23:24:13 +0200

    5.19 toimii

---
+++ osa_5/redux-anecdotes/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+class App extends React.Component {
+
+  voteFor = (id) => (event) => {
+    event.preventDefault()
+
+    this.props.store.dispatch({
+      type: 'VOTE',
+      id: id
+    })
+  }
+
+  render() {
+    const anecdotes = this.props.store.getState()
+    return (
+      <div>
+        <h2>Anecdotes</h2>
+        {anecdotes.map(anecdote=>
+          <div key={anecdote.id}>
+            <div>
+              {anecdote.content} 
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={this.voteFor(anecdote.id)}>vote</button>
+            </div>
+          </div>
+        )}
+        <h2>create new</h2>
+        <form onSubmit={this.addNew}>
+          <div><input name="anecdote" /></div>
+          <button>create</button> 
+        </form>
+      </div>
+    )
+  }
+}
+
+export default App
