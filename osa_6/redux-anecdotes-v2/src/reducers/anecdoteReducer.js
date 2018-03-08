67b43fa45d7f075a9a59827df9346c5236170bc9
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-03-09 00:59:53 +0200

    6.2 done

--- osa_6/redux-anecdotes-v2/src/reducers/anecdoteReducer.js
+++ osa_6/redux-anecdotes-v2/src/reducers/anecdoteReducer.js
@@ -7,7 +7,7 @@ const anecdotesAtStart = [
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
 
-const getId = () => (100000*Math.random()).toFixed(0)
+const getId = () => (100000 * Math.random()).toFixed(0)
 
 const asObject = (anecdote) => {
   return {
@@ -20,18 +20,35 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (store = initialState, action) => {
-  if (action.type==='VOTE') {
-    const old = store.filter(a => a.id !==action.id)
-    const voted = store.find(a => a.id === action.id)
-
-    return [...old, { ...voted, votes: voted.votes + 1 } ]
+  switch (action.type) {
+  case 'VOTE':
+    const id = action.data.id
+    const old = store.filter(a => a.id !== id)
+    const voted = store.find(a => a.id === id)    
+  return [...old, { ...voted, votes: voted.votes + 1 }]
+  case 'CREATE':
+    return [...store, action.data]
+  default:
+    return store
   }
-  if (action.type === 'CREATE') {
+}
 
-    return [...store, { content: action.content, id: getId(), votes:0 }]
+export const giveVote = (id) => {
+  return {
+    type: 'VOTE',
+    data: { id }
   }
+}
 
-  return store
+export const newAnecdote = (content) => {
+  return {
+    type: 'CREATE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0
+    }
+  }
 }
 
 export default reducer
