21071a5bf3f96b8a9463c39b3be2ee4e705ff34d
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-03-09 00:13:33 +0200

    6.1 eslint done

--- osa_6/redux-anecdotes-v2/src/index.js
+++ osa_6/redux-anecdotes-v2/src/index.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 import store from './store'
+//import { createLogger } from 'redux-logger'
 
 
 const render = () => {
