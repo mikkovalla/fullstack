ae183e66291d904abecbe766d5cd0f984d108d8a
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-03-11 19:08:02 +0200

    6.11 done

--- osa_6/redux-anecdotes-v2/src/components/AnecdoteForm.js
+++ osa_6/redux-anecdotes-v2/src/components/AnecdoteForm.js
@@ -2,13 +2,15 @@ import React from 'react'
 import { newAnecdote } from '../reducers/anecdoteReducer'
 import { notification } from '../reducers/notificationReducer'
 import { connect } from 'react-redux'
+import anecdoteService from '../services/anecdotes'
 
 class AnecdoteForm extends React.Component {
 
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
-    this.props.newAnecdote(content)
+    const uusin = await anecdoteService.create(content)
+    this.props.newAnecdote(uusin)
     const notice = `you added ${content}`
     this.props.notification(notice)
     setTimeout(() => {
