55a691304fc5445ba8cdc52ee963f7e7e0a06ee4
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-03-11 20:44:22 +0200

    6.13

--- osa_6/redux-anecdotes-v2/src/store.js
+++ osa_6/redux-anecdotes-v2/src/store.js
@@ -1,14 +1,22 @@
-import { createStore, combineReducers } from 'redux'
+import {
+  createStore,
+  combineReducers,
+  applyMiddleware
+} from 'redux'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
+import thunk from 'redux-thunk'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
   notification: notificationReducer,
   filter: filterReducer
 })
-const store = createStore(reducer)
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk))
+
 console.log('storen tila', store.getState())
 
 export default store
