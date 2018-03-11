6cdb7fcfe3224ce2cf2880c6213fbdfcd60b6fce
Mikko Vallaskivi <mikko.vallaskivi@gmail.com>
2018-03-11 22:39:03 +0200

    6.17 done

--- osa_6/routed-anecdotes/src/App.js
+++ osa_6/routed-anecdotes/src/App.js
@@ -13,10 +13,20 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      { anecdotes.map(anecdote => <li key={anecdote.id}> <Link to={`/anecdotes/${anecdote.id}`}> {anecdote.content} </Link> </li>)}
     </ul>  
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+  return ( 
+    <div>
+      <h3>{`${anecdote.content}} by ${anecdote.author}`}</h3>
+      <p>{`has ${anecdote.votes} votes`}</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -146,6 +156,7 @@ class App extends React.Component {
             <Route exact path='/' render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route path='/create' render={() => <CreateNew addNew={this.addNew} />} />
             <Route path='/about' render={() => <About />} />
+            <Route exact path='/anecdotes/:id' render={({ match }) => <Anecdote anecdote={this.anecdoteById(match.params.id)} />} />
           </div>
         </Router>
         <Footer />
